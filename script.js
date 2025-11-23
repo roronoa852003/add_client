f256d372dfe5dc80587b875eb3a6021ef16b53b7
unknown <vedanthundare21@gmail.com>
2025-11-23 15:27:12 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-let cursor=document.querySelector('img');
-let main=document.querySelector('main');
+let elem = document.querySelectorAll('.elem');
 
-main.addEventListener('mousemove',(dets)=>{
-    cursor.style.left=dets.x+'px';
-    cursor.style.top=dets.y+'px';
+
+elem.forEach((val) => {
+    val.addEventListener('mouseenter', function () {
+
+        let head = val.querySelector('h2')
+        let mouse = val.querySelector('img');
+        head.style.fontSize = "35px"
+
+        mouse.style.opacity=1
+
+
+    })
+    val.addEventListener('mouseleave', function () {
+        let head = val.querySelector('h2')
+        let mouse = val.querySelector('img');
+        head.style.fontSize = "30px";
+        mouse.style.opacity=0;
+
+    })
+    val.addEventListener('mousemove', (dets) => {
+        let mouse = val.querySelector('img');
+
+        // elem ke andar ka mouse position
+        let x = dets.offsetX;
+        let y = dets.offsetY;
+
+        mouse.style.left = x + 'px';
+        mouse.style.top = y + 'px';
+    });
+
 })
